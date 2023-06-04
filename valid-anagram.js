ec6746a8363db9b18b2aa4f4ca5b9c31a60fb3dc
Scott Wells <45832584+scott-wells@users.noreply.github.com>
2023-06-04 13:54:02 -0700

    valid anagram solution

--- valid-anagram.js
+++ valid-anagram.js
@@ -7,24 +7,35 @@
 
 var isAnagram = function(s, t) {
     
+    if (s.length !== t.length) return false;
+
     const sMap = new Map();
     const tMap = new Map();
 
     for (let i = 0; i < s.length; ++i) {
-        if (!sMap.has(s[i])) sMap.set(s[i], 1);
+        if (!sMap.has(s[i])) {
+            sMap.set(s[i], 1);
+            continue;
+        }
         let value = sMap.get(s[i]);
         value++;
         sMap.set(s[i], value);
     }
+
     for (let i = 0; i < t.length; ++i) {
-        if (!tMap.has(t[i])) tMap.set(t[i], 1);
+        if (!tMap.has(t[i])) {
+            tMap.set(t[i], 1);
+            continue;
+        }
         let value = tMap.get(t[i]);
         value++;
         tMap.set(t[i], value);
     }
 
-    console.log("sMap: ", sMap);
-    console.log("tMap: ", tMap);
+    for (const c of sMap.keys()) {
+        if (sMap.get(c) !== tMap.get(c)) return false;
+    }
+    return true;
 };
 
-isAnagram("anagram", "nagaram");
+isAnagram("anagram", "nagara");
