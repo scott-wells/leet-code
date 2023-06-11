ed69ad67688c0a9bf5a156f0b62d9b69a1e1f1f7
Scott Wells <45832584+scott-wells@users.noreply.github.com>
2023-06-11 10:06:04 -0700

    solution to find index first occurance of string

---
+++ find-index-first-occurance-in-string.js
@@ -0,0 +1,41 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+ * or -1 if needle is not part of haystack.
+ * 
+ * Key to this problem is two pointers to keep track of the chars in haystack and needle.
+ * 
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) { 
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    while (i < haystack.length) { // loop through haystack
+        while (needle[j] === haystack[k]) { // as long as the letters match, keep looping
+            // step your pointers forward
+            k++;
+            j++;
+            if (j === needle.length) { // if you make it to end of needle, then you found match
+                console.log(i);
+                return i;
+            }
+            if (k === haystack.length) { // if you make it to end of haystack, then you didn't find match
+                console.log(-1);
+                return -1;
+            }
+        }
+        if (needle[j] !== haystack[k]) { // if letters don't match, step your pointers forward
+            i++;
+            k = i;
+            j = 0;
+        }
+    }
+    console.log(-1);
+    return -1;
+};
+strStr("rodeoride","ride");
+strStr("butttruckduck","truck");
+strStr("sadbutsad","sad");
+strStr("leetcode","leeto");
